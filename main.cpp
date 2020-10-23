3d627cf864190c5e3a6452ccf8b0d6686d8de462
zoeschmitt <zoeschmitt@hotmail.com>
2020-10-22 20:13:38 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  // parse arguments
+  
+  return 0;
+}
